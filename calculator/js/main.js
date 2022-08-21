16ffb4a7b5575becea8129103487f531b5f609d1
stewart-bryant <stewartbryant@hotmail.com>
2022-08-21 23:18:35 +0200

    added JS pseudocode

---
+++ calculator/js/main.js
@@ -0,0 +1,33 @@
+const calculator = {
+    divide() => ,
+    multiply() => ,
+    subtract() => ,
+    add() => ,
+    negative() => ,
+    percent() => ,
+    calculate() => ,
+    del() => ,
+    clear() => ,
+}
+
+//accept user inputs (number, operator, other numbers)
+    //including longer inputs
+//display inputs as user enters
+//store inputs
+//recognize inputs
+//perform calculations
+//return result
+//previous total is start of new operation unless user clears w/ 'AC' button
+//backspace should del last input
+//AC should clear all entries
+//prevent invalid inputs (start w/operator, operators next to each other, multiple decimal points, etc.)
+
+//bonus
+//mimic windows calculator output window
+    //two fields
+        //main field for input numbers
+            //char limit
+        //smaller field for operation
+            //overflow enabled
+//pressing '=' repetitively repeats last operation
+    
